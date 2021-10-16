41b33ef34b4e8c4dd77b222a9c12fa8fde52234e
AreYouScorpio <akos.schweger@gmail.com>
2021-10-16 13:56:38 +0200

    training "kiiras es beolvasas konzolrol" orai gyakolati feladat Reg

---
+++ src/main/java/classstructureio/Registration.java
@@ -0,0 +1,16 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nev:");
+        String Name = scanner.nextLine();
+        System.out.println("Email cim:");
+        String Email = scanner.nextLine();
+        System.out.println("Regisztráltál a következő néven:" + Name);
+        System.out.println("Regisztráltál a következő email címmel:" + Email);
+
+    }
+}
