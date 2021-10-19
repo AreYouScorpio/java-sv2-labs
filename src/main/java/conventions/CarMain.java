6377bee2d901f8e152980b7e5df5a33a966422b9
AreYouScorpio <akos.schweger@gmail.com>
2021-10-19 18:24:05 +0200

    car done

--- src/main/java/conventions/CarMain.java
+++ src/main/java/conventions/CarMain.java
@@ -18,12 +18,15 @@ public class CarMain {
         scanner.nextLine();
         System.out.println("Auto típusa? " );
         String carType = scanner.nextLine();
-        car.addModelName(carType);
+        car.setCarType(carType);
+        System.out.println("Auto modellje? " );
+        String modellName = scanner.nextLine();
+        car.addModelName(modellName);
         System.out.println("Milyen a motorja? " );
         String engineType = scanner.nextLine();
         car.setEngineType(engineType);
 
 
-        System.out.println("Az autód specifikációi: " + car );
+        System.out.println("Az autód specifikációi: típus és modell: " + car.getCarType() + " motortípus: " + car.getEngineType() + " ülések száma: " + car.getPersons() + " ajtók száma: " + car.getDoors() );
     }
 }
