1049922944f206db432d718fd7c72c38896e39df
AreYouScorpio <akos.schweger@gmail.com>
2021-10-21 12:28:45 +0200

    DivByThree done

---
+++ src/main/java/statements/DivByThree.java
@@ -0,0 +1,20 @@
+package statements;
+
+import java.util.Scanner;
+
+public class DivByThree {
+
+    public static void main(String[] args) {
+
+        int number;
+        boolean okay;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adj meg egy számot: ");
+        number = scanner.nextInt();
+        okay = number % 3 == 0 ;
+        System.out.println("Osztható hárommal?" + "\n" + okay);
+        System.out.println(number%3==0 ? "igen" : "nem");
+
+    }
+
+}
