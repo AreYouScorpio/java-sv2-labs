9a694705412d1b2e9b598989baa99fa2b208c160
AreYouScorpio <akos.schweger@gmail.com>
2021-10-22 11:37:04 +0200

    JAVA API uppercase feladat done

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,10 @@
+package javaapi;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!".toUpperCase());
+    }
+}
