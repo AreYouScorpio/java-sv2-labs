5cbb4d4cfb16ce973da5dd3ce99d2f9a1f2986aa
AreYouScorpio <akos.schweger@gmail.com>
2021-10-23 23:31:35 +0200

    Registration feladat done

--- src/main/java/registration/Registration.java
+++ src/main/java/registration/Registration.java
@@ -8,7 +8,20 @@ public class Registration {
 
         Scanner scanner = new Scanner(System.in);
         String username2 = scanner.nextLine();
+        String password1 = scanner.nextLine();
+        String password2 = scanner.nextLine();
+        String email = scanner.nextLine();
+        boolean passwordcheck;
+        passwordcheck = password1.equals(password2);
+
         UserValidator userValidator = new UserValidator();
-        System.out.println(userValidator.isValidUsername(username2));
+        System.out.println("Username OK?" + userValidator.isValidUsername(username2));
+        System.out.println("Password1 OK? " + userValidator.isValidPassword(password1));
+        System.out.println("Password2 OK? " + userValidator.isValidPassword(password2));
+        System.out.println("Passwords are the same? " + passwordcheck);
+        System.out.println("Email OK?" + userValidator.isValidEmail(email));
+
+
+
     }
 }
--- src/main/java/registration/UserValidator.java
+++ src/main/java/registration/UserValidator.java
@@ -14,7 +14,28 @@ public class UserValidator {
     public boolean isValidUsername(String username)
 
     {
-        boolean a = username.length()>0;
+        boolean a = username.length()> 0;
         return a;
     }
+    public boolean isValidPassword(String password)
+
+    {
+        boolean b = password.length()==8;
+        return b;
+    }
+
+    public boolean isValidEmail(String email)
+
+    {
+        int kukac;
+        kukac = email.indexOf("@");
+        System.out.println("Kukac helye: " + (kukac+1));
+        int pont;
+        pont = email.indexOf(".",kukac+1);
+        System.out.println("Pont helye: " + (pont+1));
+
+        boolean c = email.length()> 0 && kukac>0 && pont>kukac ;
+        return c;
+    }
+
 }
