8dc73abda550a16b78de451178d285aa7146eda9
AreYouScorpio <akos.schweger@gmail.com>
2021-10-27 23:49:24 +0200

    ObjectsMain fa

---
+++ src/main/java/objects/ObjectsMain.java
@@ -0,0 +1,67 @@
+package objects;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectsMain {
+
+    public static void main(String[] args) {
+
+
+
+        new Book();
+
+
+
+        System.out.println(new Book());
+
+        Book emptyBook;
+
+
+
+        emptyBook = null;
+
+        boolean isNull = emptybook == null;
+        System.out.println(isNull);
+
+        Book book = new Book;
+
+        System.out.println(book);
+
+        book = null;
+
+        System.out.println(book);
+
+
+        book = new Book();
+
+        System.out.println(Book);
+
+        Book anotherBook = new Book();
+
+        System.out.println(book==anotherBook);
+
+        anotherBook = book;
+
+        System.out.println(anotherBook);
+
+
+        Book [] books = {new Book(), new Book(), new Book()};
+
+        System.out.println();
+
+        List<Book> books1 = Arrays.asList(new Book(), new Book(), new Book() )
+        System.out.println(books1);
+
+        List<Book> books2 = new ArrayList<>();
+        books2.add(new Book());
+        books2.add(new Book());
+        books2.add(new Book());
+        System.out.println(books2);
+
+
+    }
+
+
+}
