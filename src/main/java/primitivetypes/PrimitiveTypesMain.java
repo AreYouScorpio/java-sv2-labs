d31c86409087702ddf3b03f5e07b3ba6d278fbfa
AreYouScorpio <akos.schweger@gmail.com>
2021-10-31 19:57:42 +0100

    egész szémok és logikai értékek feladatok - done

--- src/main/java/primitivetypes/PrimitiveTypesMain.java
+++ src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -1,6 +1,8 @@
 package primitivetypes;
 
 
+import compositionlist.Books;
+
 import java.io.OutputStream;
 
 public class PrimitiveTypesMain {
@@ -26,6 +28,26 @@ public class PrimitiveTypesMain {
         System.out.println(i);
         Integer j = Integer.parseInt("2");
         System.out.println(j);
+
+        boolean k = true;
+        System.out.println(k);
+        Boolean l = true;
+        System.out.println(l);
+        boolean m = Boolean.valueOf(true);
+        System.out.println(m);
+        Boolean n = Boolean.valueOf(true);
+        System.out.println(n);
+        boolean o = Boolean.parseBoolean("TrUe");
+        System.out.println(o);
+        Boolean p = Boolean.parseBoolean("TrUe");
+        System.out.println(p);
+        boolean q = Boolean.parseBoolean("TreU");
+        System.out.println(q);
+        Boolean r = Boolean.parseBoolean("TreU");
+        System.out.println(r);
+
+
+
     }
 
 
