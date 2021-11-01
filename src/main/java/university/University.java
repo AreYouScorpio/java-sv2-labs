a126f69c913b692536442e20f439fd5ab0054142
AreYouScorpio <akos.schweger@gmail.com>
2021-11-01 22:23:28 +0100

    University feladat - done

---
+++ src/main/java/university/University.java
@@ -0,0 +1,15 @@
+package university;
+
+public enum University {
+    BME("Budapesti Muszaki Egyetem"), SZE("Szechenyi Istvan Egyetem"), BKE("Bpi Kozgaz Egyetem")   ;
+
+    String name;
+
+    University(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/university/UniversityMain.java
@@ -0,0 +1,18 @@
+package university;
+
+public class UniversityMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(University.BME);
+        System.out.println(University.BME.getName());
+
+        System.out.println(University.BKE);
+        System.out.println(University.BKE.getName());
+
+        System.out.println(University.SZE);
+        System.out.println(University.SZE.getName());
+
+
+    }
+}
