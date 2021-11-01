83d8f4feec53053b04428ca5511a3710c260e0ff
AreYouScorpio <akos.schweger@gmail.com>
2021-11-01 17:16:20 +0100

    solarsystem feladat - done

---
+++ src/main/java/solarsystem/SolarSystem.java
@@ -0,0 +1,15 @@
+package solarsystem;
+
+
+public enum SolarSystem {
+    VENUSZ(1), PLUTO(2), FOLD(3), MARS(4)    ;
+    private int moons;
+
+    SolarSystem(int moons) {
+        this.moons = moons;
+    }
+
+    public int getMoons() {
+        return moons;
+    }
+}
