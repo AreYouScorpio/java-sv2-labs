67756844f7ac00bc5072fc3625d334206c089337
AreYouScorpio <akos.schweger@gmail.com>
2021-11-01 15:13:55 +0100

    whitespace feladat - done

---
+++ src/main/java/chars/Whitespace.java
@@ -0,0 +1,26 @@
+package chars;
+
+import java.sql.Array;
+
+public class Whitespace {
+
+    public String makeWhitespaceToStar(String word) {
+        int length = word.length();
+        char[] wordInArray = word.toCharArray();
+        for (int i = 0; i < length; i++) {
+            if (Character.isWhitespace(wordInArray[i])) {
+                wordInArray[i] = '*';
+            }
+            ;
+        }
+        String result = new String(wordInArray);
+        return result;
+    }
+
+
+
+    public static void main(String[] args) {
+        Whitespace whitespace = new Whitespace();
+        System.out.println(whitespace.makeWhitespaceToStar("sziaa helloo hogy vaaagy"));
+    }
+}
