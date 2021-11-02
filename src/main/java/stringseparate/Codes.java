5ccf1aa84fabc87a620df879acb2aa63f0773d88
AreYouScorpio <akos.schweger@gmail.com>
2021-11-02 13:08:43 +0100

    codes done

--- src/main/java/stringseparate/Codes.java
+++ src/main/java/stringseparate/Codes.java
@@ -6,22 +6,25 @@ import java.util.List;
 
 public class Codes
 {
-    List<String> originalList = new ArrayList<>();
-
-    List<String> newList = new ArrayList<>();
-
-    public String getCodesStartWithLetter(List<String> originalList){
+    // List<String> originalList = new ArrayList<>();
 
 
-        for (int i = 0; i< originalList.size(); i++){
-            if (Character.isAlphabetic(originalList.get(i).charAt(0))) {
-                newList.add(originalList.get(i));
-            }
-        }
-        return newList.toString();
-
+    public Codes() {
     }
 
+    public List<String> getCodesStartWithLetter(List<String> originalList) {
+        List<String> newList = new ArrayList<>();
+
+        for (int i = 0; i < originalList.size(); i++) {
+
+            if (Character.isAlphabetic(originalList.get(i).charAt(0))) {
+                newList.add(originalList.get(i));
+
+            }
+        }
+        return newList;
+
+    }
     public static void main(String[] args) {
         Codes codes = new Codes();
         List<String> originalList = Arrays.asList("jdlasj", "8z897", "s122", "kkllk222");
