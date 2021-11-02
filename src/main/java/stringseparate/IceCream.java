0d5bb2521ac12ee5dc2aa9467973a13b7f1f7cdd
AreYouScorpio <akos.schweger@gmail.com>
2021-11-02 10:01:33 +0100

    icecream done

--- src/main/java/stringseparate/IceCream.java
+++ src/main/java/stringseparate/IceCream.java
@@ -1,4 +1,21 @@
 package stringseparate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+
 public class IceCream {
+    public static void main(String[] args) {
+
+        List<String> icecreams = Arrays.asList("alma", "málna", "eper");
+
+        for (int i = 0; i < icecreams.size(); i++) {
+            System.out.print(icecreams.get(i)+", ");
+
+        }
+
+    }
 }
