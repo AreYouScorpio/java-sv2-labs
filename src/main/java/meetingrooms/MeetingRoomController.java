59a5c3bfd6ccef227b99df468123d1c9a4019054
AreYouScorpio <akos.schweger@gmail.com>
2021-11-04 09:45:09 +0100

    projektfeladat .. 1. menu, 9. menü OK .. working on 2nd menu..

--- src/main/java/meetingrooms/MeetingRoomController.java
+++ src/main/java/meetingrooms/MeetingRoomController.java
@@ -51,6 +51,15 @@ public class MeetingRoomController {
 
         }
 
+        if (menuChoice==2) {
+        System.out.println("Az új teljes tárgyaló lista adatai: ");
+        for (int i=0; i<meetingRoom.size(); i++){
+            System.out.println(Office.meetingRoom.get(i).getName());
+            System.out.println(Office.meetingRoom.get(i).getLength());
+            System.out.println(Office.meetingRoom.get(i).getWidth());
+        }
+
+
         if (menuChoice==9) {
             System.out.println("9");
             System.exit(0);
--- src/main/java/meetingrooms/Office.java
+++ src/main/java/meetingrooms/Office.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Office {
 
-    List<MeetingRoom> meetingRoom = new ArrayList<>();
+    public List<MeetingRoom> meetingRoom = new ArrayList<>();
 
     public void addMeetingRoom(String name, int length, int width){
 
@@ -13,13 +13,10 @@ public class Office {
 
         meetingRoom.add(newOffice);
 
+
         System.out.println("Az új tárgyaló hozzáadva. Adatai: " + newOffice.getName() + " " + newOffice.getLength() + " " + newOffice.getWidth());
-        System.out.println("Az új teljes tárgyaló lista adatai: ");
-        for (int i=0; i<meetingRoom.size(); i++){
-            System.out.println(meetingRoom.get(i).getName());
-            System.out.println(meetingRoom.get(i).getLength());
-            System.out.println(meetingRoom.get(i).getWidth());
-        }
+
+
             ;
 
     }
