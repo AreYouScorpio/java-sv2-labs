09b1a3067f53734e4b564db0b4f07c096c027bb3
AreYouScorpio <akos.schweger@gmail.com>
2021-11-24 15:35:54 +0100

    transaction task done

---
+++ src/main/java/algorithmssum/transactions/Transaction.java
@@ -0,0 +1,30 @@
+package algorithmssum.transactions;
+
+public class Transaction {
+    String accountNumber;
+    TransactionOperation transactionOperation;
+    int amount;
+
+    public Transaction(String accountNumber, TransactionOperation transactionOperation, int amount) {
+        this.accountNumber = accountNumber;
+        this.transactionOperation = transactionOperation;
+        this.amount = amount;
+    }
+
+
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public TransactionOperation getTransactionOperation() {
+        return transactionOperation;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+
+
+}
